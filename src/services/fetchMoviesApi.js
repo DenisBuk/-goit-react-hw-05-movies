30388c41cecc6f95fcd507b64de8c525c6d6479e
DenisBuk <79395480+DenisBuk@users.noreply.github.com>
2023-02-09 14:50:01 +0200

    fetchApi

--- src/services/fetchMoviesApi.js
+++ src/services/fetchMoviesApi.js
@@ -1,1 +1,39 @@
 import axios from 'axios'
+
+const KEY = '8f097ac1772f5d24a794202a251ed2ca';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+export const getTrandingMovies = async () => { 
+    const response = await axios.get(
+        `${BASE_URL}/trending/all/day?api_key=${KEY}`
+    );
+    return response.data;
+};
+
+export const getMoviesQuerry = async query => { 
+    const response = await axios.get(
+        `${BASE_URL}search/movie?api_key=${KEY}&query=${query}&language=en-US&page=1&include_adult=false`
+    );
+    return response.data;
+};
+
+export const getMovieById = async id => { 
+    const response = await axios.get(
+        `${BASE_URL}movie/${id}?api_key=${KEY}&language=en-US`
+    );
+    return response.data;
+};
+
+export const getMovieCredits = async id => { 
+    const response = await axios.get(
+        `${BASE_URL}movie/${id}/credits?api_key=${KEY}&language=en-US`
+    );
+    return response.data.credits;
+};
+
+export const getMovieReviews = async id => { 
+    const response = await axios.get(
+        `${BASE_URL}movie/${id}/reviews?api_key=${KEY}&language=en-US&page=1`
+    );
+    return response.data.results;
+};
